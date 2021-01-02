c1ea6cc802dffeef0fcc85881224dfc891252695
nocturno <55927921+nocturnooo@users.noreply.github.com>
2021-01-02 23:08:30 +0200

    +update

--- arg/hideout/login/script.js
+++
@@ -1,5 +0,0 @@
-function submit() {
-    if (document.getElementById('username').value == "GURL:PNAF:RRLB:HERI:ELZB-IR" &&
-        document.getElementById('password').value == "1616" &&
-        document.getElementById('refferal').value == "VEXj4)GSeL-=_\\L") window.location.href = "../../../lore/documents/ved/";
-}
