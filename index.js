1751a8af49b74d95650c308a43cdce47751ca373
nocturno <55927921+nocturnooo@users.noreply.github.com>
2021-01-04 14:30:18 +0200

    +8080

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 app.use(express.static('www'));
-var port = process.env.port || 8080;
+var port = 8080;
 app.listen(port, function(){
     console.log('Listening on port ' + port); 
 });
